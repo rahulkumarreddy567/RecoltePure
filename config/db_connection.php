681fb15127416bc07be65971aa12dd214b0cad78
rahulkumarreddy567 <152024364+rahulkumarreddy567@users.noreply.github.com>
2026-01-20 05:56:42 +0530

    Add DB_PORT support to db_connection.php

--- config/db_connection.php
+++ config/db_connection.php
@@ -3,8 +3,9 @@ $servername = getenv('DB_HOST') ?: 'localhost';
 $username = getenv('DB_USER') ?: 'root';
 $password = getenv('DB_PASS') ?: '';
 $database = getenv('DB_NAME') ?: 'recoltepure';
+$port = getenv('DB_PORT') ?: 3306;
 
-$db = new mysqli($servername, $username, $password, $database);
+$db = new mysqli($servername, $username, $password, $database, (int) $port);
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
